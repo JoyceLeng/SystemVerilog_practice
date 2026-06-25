9093ff5240162e25816863db09f23ebb22a91b8a
JoyceLeng <joyceleng8013@gmail.com>
2026-06-25 11:40:59 +0100

    Add 4-to-2 encoder module
    Implement a 4-to-2 encoder using combinational logic.

---
+++ combinational_logic/encoder4to2.sv
@@ -0,0 +1,17 @@
+//Encoder: a combination circuit tbat performs the inverse operation of a decoder
+module encoder4to2
+(input logic [3:0]in,
+ output logic [1:0]out
+);
+
+always_comb
+  begin
+    unique case(in)
+      4'b0001: out = 2'b00;
+      4'b0010: out = 2'b01;
+      4'b0100: out = 2'b10;
+      4'b1000: out = 2'b11;
+      default: out = 2'b00;
+    endcase
+  end
+endmodule
